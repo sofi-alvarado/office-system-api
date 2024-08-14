299b8e26a5e5353f14313e20db5ad370df7351eb
Sofía Alvarado <alvaradosofiaH@outlook.com>
2024-08-14 10:07:23 -0600

    Adding query to show all employees and get an employee by id.

--- models/Employees.php
+++ models/Employees.php
@@ -1,15 +1,29 @@
 <?php
 class Employess {
-    private $pdo;
+  private $pdo;
 
-    public function __construct($pdo) {
-      $this->pdo = $pdo;
-    }
+  public function __construct($pdo) {
+    $this->pdo = $pdo;
+  }
 
-    public function create($name, $lastname, $genre, $employment_area, $role_id, $password) {
-      $stmt = $this->pdo->prepare("INSERT INTO employees (name, lastname, genre, employment_area) VALUES (?, ?, ?, ?)");
-      $stmt->execute([$name, $lastname, $genre, $employment_area, $role_id, password_hash($password, PASSWORD_DEFAULT)]);
-      return $this->pdo->lastInsertId();
-    }
+  // Create an employee
+  public function create($name, $lastname, $genre, $employment_area, $role_id, $password) {
+    $stmt = $this->pdo->prepare("INSERT INTO employees (name, lastname, genre, employment_area) VALUES (?, ?, ?, ?)");
+    $stmt->execute([$name, $lastname, $genre, $employment_area, $role_id, password_hash($password, PASSWORD_DEFAULT)]);
+    return $this->pdo->lastInsertId();
+  }
+
+  // Show all employees
+  public function getAll() {
+    $stmt = $this->pdo->query("SELECT employees * FROM employees");
+    return $stmt->fetchAll();
+  }
+
+  // Get an employee by id
+  public function getById($id) {
+    $stmt = $this->pdo->prepare("SELECT * FROM employees WHERE id = ?");
+    $stmt->execute([$id]);
+    return $stmt->fetch();
+  }
 }
 ?>
